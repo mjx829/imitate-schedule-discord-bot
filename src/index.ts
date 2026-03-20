d0731516b37ffc391e2740406f09a21966bf1a20
mjx829 <sbtk666+buin@gmail.com>
2026-03-20 15:07:27 +0900

    feat: 日付の照合と条件に該当するセルの取得を実装

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { log } from "@/utils/logger";
+import { initDoc, fetchSheet, fetchDateColumnsRange, fetchCellsFromRange } from "@/spreadsheet";
 
 process.on("uncaughtException", (e) => {
     log.write("ERROR", `uncaught exception. (${(e as Error).message})(${(e as Error).stack})`);
@@ -11,9 +12,18 @@ process.on("unhandledRejection", (reason) => {
     process.exit(1);
 });
 
-function main(): void {
+async function main(): Promise<void> {
     try {
         log.write("INFO", "starting...");
+
+        const doc = await initDoc();
+        const sheet = await fetchSheet(doc, "2026");
+
+        const cellsRange = await fetchDateColumnsRange(sheet, "A:A", "3/2");
+        console.log(cellsRange);
+        const cells = await fetchCellsFromRange(sheet, cellsRange.offset, cellsRange.range);
+
+        console.log(cells);
 
         process.on("SIGTERM", () => { 
             process.exit(0);
--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -1,11 +1,73 @@
-import { GoogleSpreadsheet } from "google-spreadsheet";
+import { GoogleSpreadsheet, GoogleSpreadsheetWorksheet } from "google-spreadsheet";
 import { CONFIG } from "@/const";
 import { log } from "@/utils/logger";
 import { auth } from "@/auth";
 
-const initDoc = async (): Promise<GoogleSpreadsheet> => {
+type RowRange = {
+    offset: number,
+    range: number,
+}
+
+export const initDoc = async (): Promise<GoogleSpreadsheet> => {
     const doc = new GoogleSpreadsheet(CONFIG.SPERADSHEET.SHEET_ID, auth());
     await doc.loadInfo();
 
+    log.write("INFO", "doc wad loaded from spreadsheet.");
     return doc;
 }
+
+export const fetchSheet = async (doc: GoogleSpreadsheet, sheetTitle: string): Promise<GoogleSpreadsheetWorksheet> => {
+    const sheet = doc.sheetsByTitle[sheetTitle];
+    if (!sheet) throw new Error(`Sheet ${sheetTitle} was not found`);
+    await sheet.loadHeaderRow();
+
+    log.write("INFO", "sheet was loaded from spreadsheet");
+    return sheet;
+}
+
+export const fetchDateColumnsRange = async (sheet: GoogleSpreadsheetWorksheet, index: string, date: string): Promise<RowRange> => {
+    await sheet.loadCells(index);
+
+    const MAX_EMPTY_CELLS_COUNT = 100;
+    let emptyCellsCount = 0;
+
+    let isCollecting = false;
+    let offset = 0;
+    let range = 0;
+
+    const HEADER_ROW_OFFSET = -1;
+
+    for (let i = 0; i < sheet.rowCount; i++) {
+        const cell = sheet.getCell(i, 0);
+        if (!cell) break;
+        const value = cell.value;
+        
+        if (value) {
+            emptyCellsCount = 0;
+        } else {
+            emptyCellsCount++;
+        }
+        if (emptyCellsCount > MAX_EMPTY_CELLS_COUNT) break;
+
+        if (value === date && !isCollecting) {
+            isCollecting = true;
+            offset = i + HEADER_ROW_OFFSET;
+        }
+        if (!isCollecting) continue;
+
+        if (value && value !== date) {
+            break;
+        } else {
+            range++;
+        }
+    }
+
+    log.write("DEBUG", "");
+    return { offset, range };
+}
+export const fetchCellsFromRange = async (sheet: GoogleSpreadsheetWorksheet, offset: number, range: number) => {
+    const rows = await sheet.getRows({ offset: offset, limit: range });
+
+    return rows;
+}
+
